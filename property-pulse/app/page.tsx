6f4a9dfeaca17c61c6058faf8f23261d678758e0
Jawed <jawedalizada.dev@gmail.com>
2026-01-03 16:34:47 +0700

    Added: Recent properties section

--- property-pulse/app/page.tsx
+++ property-pulse/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 }
