46011b388f191b2612a92631d6c816f33aa4a617
Jawed <jawedalizada.dev@gmail.com>
2026-01-18 14:40:38 +0700

    Finished: Search functionality

--- property-pulse/app/properties/search-results/page.tsx
+++ property-pulse/app/properties/search-results/page.tsx
@@ -4,6 +4,11 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import type { Property } from "@/types/types";
+import Spinner from "@/components/Spinner";
+import ProperyCard from "@/components/ProperyCard";
+import SearchForm from "@/components/SearchForm";
+import Link from "next/link";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 
 const SearchResultsPage = () => {
   const [properties, setProperties] = useState<Property[]>([]);
@@ -15,7 +20,7 @@ const SearchResultsPage = () => {
   const fetchSearchResults = async () => {
     try {
       const res = await fetch(
-        `/api/properties/search?location=${location?.toLowerCase()}&propertyType=${propertyType?.toLocaleLowerCase()}`
+        `/api/properties/search?location=${location?.toLowerCase()}&propertyType=${propertyType?.toLocaleLowerCase()}`,
       );
       const data = await res.json();
 
@@ -31,11 +36,43 @@ const SearchResultsPage = () => {
 
   useEffect(() => {
     fetchSearchResults();
-  }, []);
+  }, [location, propertyType]);
+
+  if (loading) return <Spinner loading={loading} />;
+
   return (
-    <div>
-      <p>Search results page</p>
-    </div>
+    <section>
+      <div className="bg-blue-700 py-6">
+        <SearchForm />
+      </div>
+
+      <div className="px-10 mt-10">
+        <Link
+          className="flex items-center gap-2 text-blue-700 cursor-pointer"
+          href={"/properties"}
+        >
+          <FaArrowAltCircleLeft /> Back to properties
+        </Link>
+      </div>
+
+      <div className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.length === 0 ? (
+              <div className="w-full h-screen">
+                <p className="w-full text-center text-4xl font-medium">
+                  No properties found
+                </p>
+              </div>
+            ) : (
+              properties.map((property: Property) => (
+                <ProperyCard key={property?._id} property={property} />
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
